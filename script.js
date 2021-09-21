390514080125a646c8261131f2805c18a8d41449
Monishka <monishkagohil1194@gmail.com>
2021-09-21 15:36:30 +0200

    pushing files from desktop

---
+++ script.js
@@ -0,0 +1,8 @@
+const colors = ["Orangered","Blue","Yellow","Green","Orange", "Cyan","DodgerBlue", "Violet", "Navy", "Purple", "YellowGreen", "OrangeRed", "SlateBlue", "Salmon", "Crimson", "HotPink", "Magenta"];
+const index = 0;
+
+document.querySelector("#submit").addEventListener("click", () => {
+  if(index > colors.length - 1)
+    index = 0;
+  document.body.style.backgroundColor = colors[index++];  
+});
